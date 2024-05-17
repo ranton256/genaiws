31f58601743ad81e6526105667f53d9787565444
Richard Anton <ranton@ranton.org>
2024-05-16 19:57:19 -0700

    Cleanup comment

--- test_rag_eval.py
+++ test_rag_eval.py
@@ -40,7 +40,6 @@ faithfulness = FaithfulnessMetric()
 dataset = EvaluationDataset()
 dataset.add_test_cases_from_csv_file(
     file_path="rag_results.csv",
-    #file_path="test_data_for_eval.csv",
     input_col_name="question",
     actual_output_col_name="answer",
     expected_output_col_name="ground_truth",
